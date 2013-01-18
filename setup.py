a707201bc2c39a0f01176f06de3d9e44d9fe9ebc
Paul Osborne <paul.osborne@spectrumdsi.com>
2013-01-18 13:07:04 -0600

    Issue 14: Incporporate support for file retrieval & where_used query

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='anvil',
-      version='0.1',
+      version='0.2',
       description='Anvil works with Kiln to provide programmatic access to data',
       packages=['anvil'],
       install_requires=['requests'],
